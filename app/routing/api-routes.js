bab73ad4cd0e2b0f4071a4f606a9ddef0e957356
bryantsands <bryantsands@bryantsandstekiMacBook-Pro.local>
2017-01-12 17:23:03 -0800

    dependencies

---
+++ app/routing/api-routes.js
@@ -0,0 +1,8 @@
+var app = express();
+
+
+// get route with url /api/friends this is to display JSON of all possible friends.
+// post routes /api/friends handles incoming survey results. this route will also be used to handle the compatability logic.
+app.get('/api/friends', function(req, res){
+    res
+});
